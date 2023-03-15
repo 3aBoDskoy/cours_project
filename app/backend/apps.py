6a256535186f5dacfec5087a5f293e0385d17186
Gleb Sviridoff <gas53@bk.ru>
2023-03-15 11:01:37 +0300

    update project

--- app/backend/apps.py
+++ app/backend/apps.py
@@ -1,6 +1,15 @@
+<<<<<<<< HEAD:app/backend/apps.py
 from django.apps import AppConfig
 
 
 class BackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend'
+========
+from django.apps import AppConfig
+
+
+class FrontendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'frontend'
+>>>>>>>> b4b5c7e (update project):app/frontend/apps.py
